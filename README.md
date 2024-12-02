a1d10412368ab0af0526d97eea6736c0eff061b7
Valerie Mauduit <valerie.mauduit@pricefx.com>
2024-12-02 16:44:12 +0100

    Day 02

--- README.md
+++ README.md
@@ -26,3 +26,47 @@ in the right directory
 
 Once again, my main point was that I hadn't prepared my environment yet. Moreover, I prepared a lib but didn't remember
 how to use it (read data facilities)... I need to document it!
+
+### Day 2 - Red-Nosed Reports
+
+Star 1: didn't understand why I couldn't right this:
+```python
+return min(differences) > 0 & max(differences) < 4
+```
+Needed to write this instead:
+```python
+if min(differences) > 0:
+    return max(differences) < 4
+return False
+```
+
+## Summary
+
+**TODO UPDATE!**
+
+I am a little frustrated to stop. I feel like the 2019 AoC: I had just finished, in time, all the 2020 one, so I decided
+to do it after. But I kept blocked on one day and I dropped. Here, same feeling: even if I could finish some other
+stars:
+
+- I am pretty sure that some will still resist
+- I will spend all my holiday week on it
+
+What went well: 
+- my test driven development. Not perfect, I will prepare something better for next year, with less
+rigidity. But it was still nice to have it.
+- speaking with my husband. We had different ideas. Speaking helped us to write our code with many good tips.
+- Ladies of Code leaderboard, with ladies participating. Because we weren't only Paola and me competing like in 2020,
+  but also other ladies continue day after day (not enough). It is nice to be together on the same game during a month.
+
+What went wrong:
+
+- I need to have some hand-made functions to deal with 2D maps. Some that I perfectly know and that I can append in 
+  case of necessity.
+- I need to have some way, already written, to deal with graphs! And maybe this story of Depth Search First.
+- my husband doing the AoC with a mechanical keyboard. I was way too stressed because of it. I mustn't be in this kind
+  of competition mode! Is it so important to enjoy doing it. (Precision: it is *my* fault, not my husband's. It is OK
+  for him to have a mechanical keyboard and to use it.)
+- I was too slow to enjoy the reddit feed https://www.reddit.com/r/adventofcode/ because I really prefer to go on it
+  when I have finished the exercises of the day.
+
+My idea: do, quietly, the 2021 during the year, to prepare the 2023 with a good tooling.
---
+++ all_days/day02.py
@@ -0,0 +1,60 @@
+# Day2: Red-Nosed Reports
+
+# First star: The engineers are trying to figure out which reports are safe. The Red-Nosed reactor safety systems can
+# only tolerate levels that are either gradually increasing or gradually decreasing. So, a report only counts as safe if
+# both of the following are true:
+# - The levels are either all increasing or all decreasing.
+# - Any two adjacent levels differ by at least one and at most three.
+# How many reports are safe?
+
+# Second star: The Problem Dampener is a reactor-mounted module that lets the reactor safety systems tolerate a single
+# bad level in what would otherwise be a safe report. It's like the bad level never happened!
+# Now, the same rules apply as before, except if removing a single level from an unsafe report would make it safe, the
+# report instead counts as safe.
+# How many reports are now safe?
+
+
+import os
+import sys
+current_dir = os.path.dirname(os.path.realpath(__file__))
+parent_dir = os.path.dirname(current_dir)
+sys.path.append(parent_dir)
+
+from AoC_tools.read_data import read_data
+
+
+def is_safe(report):
+    transformed_report = report.copy()
+    transformed_report.sort()
+    reversed_report = report.copy()
+    reversed_report.reverse()
+    if transformed_report in [report, reversed_report]:
+        differences = [abs(report[index + 1] - report[index]) for index in range(len(report) - 1)]
+        if min(differences) > 0:
+            return max(differences) < 4
+        return False
+    return False
+
+
+def is_safe_with_tolerance(report):
+    return any([is_safe(report[0:i] + report[(i+1):]) for i in range(len(report))])
+
+
+def count_safe(reports, with_tolerance=False):
+    if with_tolerance:
+        return sum([is_safe_with_tolerance(report) for report in reports])
+    return sum([is_safe(report) for report in reports])
+
+
+def run(data_dir, star):
+    data = read_data(f'{data_dir}/input-day02.txt', split=' ')
+
+    if star == 1:  # The final answer is: 202
+        solution = count_safe(data)
+    elif star == 2:  # The final answer is:
+        solution = count_safe(data, with_tolerance=True)
+    else:
+        raise Exception('Star number must be either 1 or 2.')
+
+    print(f'The solution (star {star}) is: {solution}')
+    return solution
