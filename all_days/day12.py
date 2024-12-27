37ac9dd7cbb5218e1f17ec54b983c99dac381a83
Valerie Mauduit <valerie.mauduit@pricefx.com>
2024-12-27 16:11:36 +0100

    Day 12 all stars

--- all_days/day12.py
+++ all_days/day12.py
@@ -13,54 +13,104 @@
 # for every region on the map.
 # What is the total price of fencing all regions on your map?
 
-# Second star: description
+# Second star: Fortunately, the Elves are trying to order so much fence that they qualify for a bulk discount!
+# Under the bulk discount, instead of using the perimeter to calculate the price, you need to use the number of sides
+# each region has. Each straight section of fence counts as a side, regardless of how long it is.
+# What is the new total price of fencing all regions on your map?
 
 import os
 import sys
+import re
+
 current_dir = os.path.dirname(os.path.realpath(__file__))
 parent_dir = os.path.dirname(current_dir)
 sys.path.append(parent_dir)
 
 from AoC_tools.read_data import read_data
 from AoC_tools.work_with_maps import AocMap
+from AoC_tools.work_with_lists import transpose
 
 
-def plot_coords(garden, position, plant, all_points=None):
-    if all_points is None:
-        all_points = [position]
-    plot_neighbours = [
-        coord
-        for coord in garden.get_neighbours_coordinates(diagonals=False)
-        if (garden.get_point(coord) == plant) & (coord not in all_points)
-    ]
-    if not plot_neighbours:
-        return all_points
-    else:
-        all_points + [plot_coords(garden, neighbour, plant, all_points) for neighbour in plot_neighbours]
-
-    return ['area', 'perimeter', ['coords']]
+def calculate_perimeter(coordinates):
+    perimeter = 0
+    for coord in coordinates:
+        neighbours_in_parcel = [c for c in get_neighbours(coord) if c in coordinates]
+        perimeter += 4 - len(neighbours_in_parcel)
+    return perimeter
 
 
-def fencing_price(data):
+def calculate_sides(coordinates):
+    xmin, xmax = min([coord // 1000 for coord in coordinates]) - 1, max([coord // 1000 for coord in coordinates]) + 1
+    ymin, ymax = min([coord % 1000 for coord in coordinates]) - 1, max([coord % 1000 for coord in coordinates]) + 1
+    lines = [[1000 * x + y for x in range(xmin, xmax + 1)] for y in range(ymin, ymax + 1)]
+    columns = transpose(lines)
+    border_count = 0
+    for line in range(1, ymax - ymin + 1):
+        borders = ''.join([
+            str((lines[line - 1][c] in coordinates) & (lines[line][c] not in coordinates))[0]
+            for c in range(xmax - xmin + 1)
+        ])
+        border_count += len(re.findall('T+', borders))
+        borders = ''.join([
+            str((lines[line - 1][c] not in coordinates) & (lines[line][c] in coordinates))[0]
+            for c in range(xmax - xmin + 1)
+        ])
+        border_count += len(re.findall('T+', borders))
+    for col in range(1, xmax - xmin + 1):
+        borders = ''.join([
+            str((columns[col - 1][l] in coordinates) & (columns[col][l] not in coordinates))[0]
+            for l in range(ymax - ymin + 1)
+        ])
+        border_count += len(re.findall('T+', borders))
+        borders = ''.join([
+            str((columns[col - 1][l] not in coordinates) & (columns[col][l] in coordinates))[0]
+            for l in range(ymax - ymin + 1)
+        ])
+        border_count += len(re.findall('T+', borders))
+    return border_count
+
+
+def get_neighbours(int_coords):
+    x0, y0 = int_coords // 1000, int_coords % 1000
+    return [1000 * x + y for x, y in zip([x0 - 1, x0 + 1, x0, x0], [y0, y0, y0 - 1, y0 + 1])]
+
+
+def fencing_price(data, with_discount=False):
     garden = AocMap(data)
+    # Create all the parcels
+    parcels = []
+    for plant in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ':
+        plant_coords = [c[0] * 1000 + c[1] for c in garden.get_marker_coords(plant)]
+        while len(plant_coords) > 0:
+            parcel = [plant_coords[0]]
+            plant_coords = plant_coords[1:]
+            neighbours = parcel
+            while len(neighbours) > 0:
+                next_neighbours = []
+                for coord in neighbours:
+                    next_neighbours += get_neighbours(coord)
+                neighbours = list(set([v for v in next_neighbours if v in plant_coords]))
+                parcel = parcel + neighbours
+                plant_coords = [c for c in plant_coords if c not in neighbours]
+            parcels += [parcel]
+    # Get metrics of the parcels and calculate price
     price = 0
-    for x in range(garden.width):
-        for y in range(garden.height):
-            plant = garden.get_point([x, y])
-            if plant != '.':
-                area, perimeter, coords = plot_metrics(garden, [x, y], plant)
-                price += area * perimeter
-                garden.set_points(coords, '.')
+    if with_discount:
+        for parcel in parcels:
+            price += len(parcel) * calculate_sides(parcel)
+    else:
+        for parcel in parcels:
+            price += len(parcel) * calculate_perimeter(parcel)
     return price
 
 
 def run(data_dir, star):
-    data = read_data(f'{data_dir}/input-day12.txt')
+    data = read_data(f'{data_dir}/input-day12.txt', numbers=False)
 
-    if star == 1:  # The final answer is:
+    if star == 1:  # The final answer is: 1461752
         solution = fencing_price(data)
-    elif star == 2:  # The final answer is:
-        solution = my_func(data)
+    elif star == 2:  # The final answer is: 904114
+        solution = fencing_price(data, with_discount=True)
     else:
         raise Exception('Star number must be either 1 or 2.')
 
--- tests/test_day12.py
+++ tests/test_day12.py
@@ -5,25 +5,50 @@ current_dir = os.path.dirname(os.path.realpath(__file__))
 parent_dir = os.path.dirname(current_dir)
 sys.path.append(parent_dir)
 
-from all_days import day00
+from all_days import day12
 
 
 def test_sets():
     return [
         {
             'number': 1,
+            'input': ['AAAA', 'BBCD', 'BBCC', 'EEEC'],
+            'expected1': 140,
+            'expected2': 80
+        },
+        {
+            'number': 2,
+            'input': ['OOOOO', 'OXOXO', 'OOOOO', 'OXOXO', 'OOOOO'],
+            'expected1': 772,
+            'expected2': 436
+        },
+        {
+            'number': 3,
             'input': [
                 'RRRRIICCFF', 'RRRRIICCCF', 'VVRRRCCFFF', 'VVRCCCJFFF', 'VVVVCJJCFE', 'VVIVCCJJEE', 'VVIIICJJEE',
                 'MIIIIIJJEE', 'MIIISIJEEE', 'MMMISSJEEE'
             ],
-            'expected1': [],
-            'expected2': []
+            'expected1': 1930,
+            'expected2': 1206
         },
+        {
+            'number': 4,
+            'input': ['EEEEE', 'EXXXX', 'EEEEE', 'EXXXX', 'EEEEE'],
+            'expected1': 692,
+            'expected2': 236
+        },
+        {
+            'number': 5,
+            'input': ['AAAAAA', 'AAABBA', 'AAABBA', 'ABBAAA', 'ABBAAA', 'AAAAAA'],
+            'expected1': 1184,
+            'expected2': 368
+        },
+
     ]
 
 
 def test_first_star(test_data, expected):
-    solution = day00.my_func(test_data)
+    solution = day12.fencing_price(test_data)
     if solution != expected:
         print("Your output is:")
         print(solution)
@@ -32,7 +57,7 @@ def test_first_star(test_data, expected):
 
 
 def test_second_star(test_data, expected):
-    solution = day00.my_func(test_data)
+    solution = day12.fencing_price(test_data, with_discount=True)
     if solution != expected:
         print("Your output is:")
         print(solution)
